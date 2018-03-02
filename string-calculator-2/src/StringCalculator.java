f389a0cf110a373dbb4681ef586ec59977dbb774
Fredrik Svahn <frsvahn@kth.se>
2018-03-02 21:47:27 +0100

    new kata added

--- string-calculator-2/src/StringCalculator.java
+++ string-calculator-2/src/StringCalculator.java
@@ -1,5 +1,40 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String s) {
-        return 0;
+        if(s.length() == 0) {
+            return 0;
+        }
+
+        String delimiterString = generateSplitDelimiterString(s);
+        s = removeDelimiterDeclaration(s);
+        String[] numbers = s.split(delimiterString);
+
+        int sum = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+           sum += Integer.parseInt(numbers[i]);
+        }
+
+        return sum;
+    }
+
+    private String generateSplitDelimiterString(String s) {
+        String delimiterString = ", *|\n";
+
+        Pattern pattern = Pattern.compile("^//\\[(.*)\\]\n");
+        Matcher matcher = pattern.matcher(s);
+
+
+        if(matcher.find()) {
+            delimiterString += "|" + matcher.group(1).replaceAll("([\\w\\W])", "\\\\\\\\$1");
+        }
+
+        return delimiterString;
+    }
+
+    private String removeDelimiterDeclaration(String string) {
+        return string.replaceAll("^//.*\n", "");
     }
 }
--- string-calculator-2/src/StringCalculatorTest.java
+++ string-calculator-2/src/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
@@ -20,5 +22,44 @@ public class StringCalculatorTest {
         assertEquals(0, sc.add("0"));
         assertEquals(5, sc.add("5"));
         assertEquals(100, sc.add("100"));
+    }
+
+    @Test
+    public void shouldReturnSumWhenStringIsTwoCommaSeparatedNumbers() {
+        assertEquals(10, sc.add("5, 5"));
+        assertEquals(25, sc.add("12, 13"));
+        assertEquals(17, sc.add("9, 8"));
+    }
+
+    @Test
+    public void shouldReturnSumOnStringWithCommaSeparatedValues() {
+        assertEquals(0, sc.add("0, 0, 0"));
+        assertEquals(150, sc.add("75, 25, 25, 25"));
+        assertEquals(597, sc.add("25, 50, 150, 250, 122"));
+    }
+
+    @Test
+    public void shouldReturnSumWhenSeparatedWithNewlinesInstead() {
+        assertEquals(0, sc.add("0\n0\n0"));
+        assertEquals(150, sc.add("75\n25\n25\n25"));
+        assertEquals(597, sc.add("25\n50\n150\n250\n122"));
+    }
+
+    @Test
+    public void shouldReturnSumWhenSeparatedWithCommasWithoutSpaces() {
+        assertEquals(0, sc.add("0,0,0,0,0"));
+        assertEquals(50, sc.add("25,15,10"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionOnIllegalSeparator() {
+        sc.add("0,\n 5");
+        sc.add("67\n,,,25");
+    }
+
+    @Test
+    public void shouldAllowAddingExtraDelimiters() {
+        assertEquals(7+3+5, sc.add("//[;]\n7;3;5"));
+        assertEquals(60+30+20+5+7+8, sc.add("//[.]\n60, 30,20\n5\n7.8"));
     }
 }
